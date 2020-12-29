2d5bbf9a2aa25c03c1743af306d41cdaba6321e8
kamiada <kamiada528@gmail.com>
2020-12-29 21:29:12 +0000

    only thing to do - space between the header and top of the page; on click copy for the hexes

--- main.js
+++ main.js
@@ -15,8 +15,12 @@ function GenerateHolders(){
     palette.map(holder => {
     const wrapper = document.getElementById("wrapper");
     const holderElement = document.createElement("div");
+    const hexText = document.createElement("span");
+    const txt = document.createTextNode(holder.colour.toString());
+    hexText.appendChild(txt);
     holderElement.setAttribute("class", "holders");
     wrapper.append(holderElement);   
+    holderElement.append(hexText);
     holderElement.style.backgroundColor = holder.colour;
 })};
 
